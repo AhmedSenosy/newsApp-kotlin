a699cef91801d2a2df79eb4b5a399474359300a5
Ahmed <ahmedsenosy.1994@gmail.com>
2021-09-11 23:30:17 +0200

    - modify ui

--- app/src/main/java/com/senosy/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/senosy/newsapp/ui/MainActivity.kt
@@ -10,13 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
 
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
--- app/src/main/java/com/senosy/newsapp/utils/BindingUtils.kt
+++ app/src/main/java/com/senosy/newsapp/utils/BindingUtils.kt
@@ -1,11 +1,13 @@
 package com.senosy.newsapp.utils
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
 
 
 object BindingUtils {
-
+    const val emptyImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.generationsforpeace.org%2Far%2Fstaff%2F%25D8%25B9%25D8%25A8%25D8%25AF%25D8%25A7%25D9%2584%25D9%2584%25D9%2587-%25D8%25A7%25D9%2584%25D8%25A8%25D9%2586%25D9%2588%25D9%258A%2Fempty-2%2F&psig=AOvVaw1nWXNZiOJxMAun3TJ6zaYU&ust=1631477277460000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJC_is7c9_ICFQAAAAAdAAAAABAD";
     @JvmStatic
     @BindingAdapter("visibleIf")
     fun changeVisibility(view: View, visible: Boolean) {
@@ -16,6 +18,12 @@ object BindingUtils {
     @BindingAdapter("invisibleIf")
     fun changeInvisibility(view: View, inVisible: Boolean) {
         view.visibility = if (inVisible) View.INVISIBLE else View.VISIBLE
+    }
+
+    @JvmStatic
+    @BindingAdapter("image")
+    fun setImage (image:ImageView , url:String?){
+        Picasso.get().load(url ?: emptyImage).into(image)
     }
 
 //    @JvmStatic
